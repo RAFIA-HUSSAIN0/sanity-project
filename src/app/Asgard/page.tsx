e294d1839131628525f98c6870e5950fcac2b04c
RAFIA-HUSSAIN0 <zonashoaib573@gmail.com>
2025-01-26 08:28:44 +0500

    error gooo ghhj

--- src/app/Asgard/page.tsx
+++ src/app/Asgard/page.tsx
@@ -12,57 +12,11 @@ import { FaFacebook } from "react-icons/fa";
 import { FaLinkedin } from "react-icons/fa";
 import { AiFillTwitterCircle } from "react-icons/ai";
 import { BsSuitHeart } from "react-icons/bs";
-import Cart from '../components/Cart';
 
-import React, { useContext } from 'react';
 
 const page = () => {
-    const { showCart, setShowCart }: any = useContext(CartContext);
-    const handleClick = () => {
-        setShowCart(!showCart)
-    }
     return (
         <div>
-            <nav className="flex justify-between items-center w-full h-[100px] sm:h-[120px] bg-[#ffffff] px-6">
-                <div className="flex w-full items-center justify-between max-w-7xl">
-                    {/* Navigation Links */}
-                    <div className="font-bold text-[#000000] space-x-[80px] absolute left-1/2 transform -translate-x-1/2">
-                        <Link href="/" className="hover:text-blue-500">Home</Link>
-                        <Link href="/Shop" className="hover:text-blue-500">Shop</Link>
-                        <Link href="/About" className="hover:text-blue-500">About</Link>
-                        <Link href="/Contact" className="hover:text-blue-500">Contact</Link>
-                    </div>
-
-                    {/* Icon Links */}
-                    <div className="hidden sm:hidden md:flex lg:flex space-x-4 lg:space-x-[60px] text-xl absolute right-16 mr-[50px]">
-                        <Link href="/Account">
-                            <span className="flex items-center">
-                                <IoPersonOutline className="hover:text-blue-500 cursor-pointer" />
-                                <IoAlert className="hover:text-blue-500 cursor-pointer ml-2" />
-                            </span>
-                        </Link>
-                        <IoSearchOutline className="hover:text-blue-500 cursor-pointer" />
-                        <FaRegHeart className="hover:text-blue-500 cursor-pointer" />
-                        <Link className='cart-icon' onClick={handleClick} href="/Asgard">
-                            <BsCart3 className="hover:text-blue-500 cursor-pointer" />
-                        </Link>
-                    </div>
-                </div>
-                {showCart && <Cart />}
-
-            </nav>
-
-
-            <div className=' flex items-center gap-[20px] h-[100px] ml-[40px] mx-[30px] '>
-                <Link className='text-[#9f9f9f]  ml-[40px] ' href="/" >Home</Link>
-                <SlArrowRight className='w-[14px] h-[8px] hover:text-blue-500 ' />
-                <Link className='text-[#9f9f9f]  ml-[40px] ' href="/">shop</Link>
-                <SlArrowRight className='w-[14px] h-[8px] hover:text-blue-500  ' />
-                <p className='text-[40px]'>|</p>
-                <Link href="/">Asgaard sofa</Link>
-
-            </div>
-
             <div className='h-[820px] mx-[30px] '>
                 <div className='flex justify-center h-[730px] mx-[30px]  '>
                     <div className='flex space-x-5 mt-[20px] w-[553px] h-[500px] mx-[30px]  '>
