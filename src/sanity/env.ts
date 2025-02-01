f5312dac2088e043d2fd512f02c2f5f0b65b9e4a
RAFIA-HUSSAIN0 <zonashoaib573@gmail.com>
2025-02-01 06:16:49 +0500

    u\b

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -11,5 +11,10 @@ export const projectId =
   'mp6smxzm'
 
 
+export const token =
+  process.env.SANITY_API_TOKEN ||
+  "skacl9ZIuQHsPdBxTrh1ll4s4iODNVhS5mbgnYnl2FPGXquIHNaHZMyvydnzjFM65Sr7Zd9zhtiWC7byPZS3wHn6YXIgJanEaCBMcTymvgr8flWpMvEpFDbdDUCy0O5owVKg0tmEP05LFKDnqQxHDd0kEUbxlMrMHeNQQAnyvAPnxdt0erKc"
+
+
 
 
